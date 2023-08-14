c5a96e051269eac141d68b36574f21b7f1778e0e
unknown <dhoouglas@hotmail.com>
2023-08-14 17:06:19 -0300

    Success page created

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContaner } from "../styles/pages/success";
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+       <SuccessContaner>
+            <h1>Compra Efetuada</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuul <strong>Douglas Lima</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+            </p>
+
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+       </SuccessContaner>
     );
 }
