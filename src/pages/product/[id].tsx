1e73166782b22c7d4ff31a9af978de4abccf63b2
unknown <dhoouglas@hotmail.com>
2023-08-14 15:34:09 -0300

    Detailed product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,59 @@
-import { useRouter } from "next/router";
+// import { GetStaticPaths } from "next";
+import { GetStaticProps } from "next";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
+import Image from "next/image";
 
-export default function Product() {
-    const { query } = useRouter()
+interface ProductProps {
+    product: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: string;
+        description: string;
+        defaultPriceId: string;
+    }
+}
+
+export default function Product({ product }: ProductProps) {
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <>
+            <ProductContainer>
+                <ImageContainer>
+
+                </ImageContainer>
+                   
+                <ProductDetails>
+                    <h1>Camiseta X</h1>
+                    <span>R$ 79,90</span>
+
+                    <p> Testando </p>
+                    <button>
+                        Comprar agora
+                    </button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     );
 }
+
+/*
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { id: 'prod_OEttoqB8L28RfE'} }
+        ],
+        fallback: 'blocking',
+    }
+}
+*/
+
+/*
+export const getStatic: GetStaticProps = async () => {
+    return {
+        props: {},
+        revalidate: 60 * 60 * 1, // 1 hour
+    }
+} 
+
+*/
