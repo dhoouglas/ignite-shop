c2a5ff56aa0697a7c0d34892f9dccaa05bb7aed0
unknown <dhoouglas@hotmail.com>
2023-08-15 12:57:31 -0300

    Cart styles complete

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,7 +1,10 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { CartButton } from "../CartButton";
-import { CartClose, CartContainer } from './styles';
+import { CartClose, CartContainer, CartFinalization, CartProduct, CartProductDetails, CartProductImage, FinalizationDetails } from './styles';
 import { X } from 'phosphor-react';
+import Image from 'next/image';
+
+import testeImage from "../../assets/Camisetas/1.png";
 
 export function Cart() {
     return (
@@ -19,8 +22,41 @@ export function Cart() {
                     <h2>Sacola de compras</h2>
                     
                     <section>
-                        <p>Parece que seu carrinho está vazio :( </p>
+                        {/*<p>Parece que seu carrinho está vazio :( </p>*/}
+
+                        <CartProduct>
+                            <CartProductImage>
+                                <Image 
+                                    width={100}
+                                    height={93}
+                                    alt=""
+                                    src={testeImage}
+                                />
+                            </CartProductImage>
+
+                            <CartProductDetails>
+                                <p>Produto 1</p>
+                                <strong>R$ 50.00</strong>
+                                <button>Remover</button>
+                            </CartProductDetails>
+                        </CartProduct>
                     </section>
+
+                    <CartFinalization>
+                        <FinalizationDetails>
+                            <div>
+                                <span>Quantidade</span>
+                                <p>2 itens</p>
+                            </div>
+
+                            <div>
+                                <span>Valor total</span>
+                                <p>R$ 100.00</p>
+                            </div>
+                        </FinalizationDetails>
+                        
+                        <button>Finalizar Compra</button>    
+                    </CartFinalization>
                 </CartContainer>
             </Dialog.Portal>
         </Dialog.Root>
